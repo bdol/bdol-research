465a86bf9d4738ed6c72edda5405300e3f9e4dc4
Brian Dolhansky <bdol@hoothoot.(none)>
2014-05-14 18:16:17 -0700

    moved eigen cula example to double precision

--- examples/cula/eig.cu
+++ examples/cula/eig.cu
@@ -22,7 +22,7 @@ void checkStatus(culaStatus status) {
 typedef struct {
   int w;
   int h;
-  culaFloat* elements;
+  culaDouble* elements;
 } Matrix;
 
 void printMatrix(Matrix M) {
@@ -34,9 +34,56 @@ void printMatrix(Matrix M) {
   }
 }
 
-void culaFloatExample() {
+void transposeSquareMatrix(Matrix A) {
+  if (A.h != A.w) {
+    printf("Error in transposeSquareMatrix(): matrix is not square!\n");
+    culaShutdown();
+    exit(EXIT_FAILURE);
+  }
+
+  for (int i=0; i<A.h; i++) {
+    for (int j=i+1; j<A.w; j++) {
+      culaDouble temp = A.elements[i*A.w + j];
+      A.elements[i*A.w + j] = A.elements[j*A.w + i];
+      A.elements[j*A.w + i] = temp;
+    }
+  }
+
+  return;
+}
+
+/**
+ * Computes the eigenvalues and eigenvectors of symmetric matrix A. Returns the
+ * eigenvectors in A (note that the original contents of A are destroyed) and
+ * the eigenvalues in w.
+ */
+void eig(Matrix A, Matrix w) {
+  // First check to make sure the dimensions of everything are correct.
+  int N = A.h;
+  if (w.h != N) {
+    printf("Error in EIG: w height is not %d.\n", N);
+    culaShutdown();
+    exit(EXIT_FAILURE);
+  }
+  if (w.w != 1) {
+    printf("Error in EIG: w height is not %d.\n", 1);
+    culaShutdown();
+    exit(EXIT_FAILURE);
+  }
+
+  // Compute the eigendecomposition
+  culaStatus status = culaDsyev('V', 'U', N, A.elements, N, w.elements);
+  checkStatus(status);
+
+  // CULA stores the result in column-major order, switch it to row-major
+  transposeSquareMatrix(A);
+
+  return;
+}
+
+void eigExample() {
   int m=4;
-  int k=4;
+  int k=5;
 
   culaStatus status;
   printf("Initializing CULA\n");
@@ -46,18 +93,18 @@ void culaFloatExample() {
   Matrix A, C;
   A.h = m; 
   A.w = k; 
-  A.elements = (culaFloat*)malloc(A.h*A.w*sizeof(culaFloat));
+  A.elements = (culaDouble*)malloc(A.h*A.w*sizeof(culaDouble));
   for (int i=0; i<m*k; i++) {
-    A.elements[i] = (culaFloat)i;
+    A.elements[i] = (culaDouble)i;
   }
   printf("A=\n");
   printMatrix(A);
 
   C.h = m; 
   C.w = m; 
-  C.elements = (culaFloat*)malloc(C.h*C.w*sizeof(culaFloat));
+  C.elements = (culaDouble*)malloc(C.h*C.w*sizeof(culaDouble));
 
-  status = culaSgemm('T', 'N', A.h, A.h, A.w, 1.0, A.elements, A.h, A.elements,
+  status = culaDgemm('T', 'N', A.h, A.h, A.w, 1.0, A.elements, A.w, A.elements,
       A.w, 0.0,
       C.elements, C.h);
   checkStatus(status);
@@ -69,10 +116,9 @@ void culaFloatExample() {
   Matrix w;
   w.h = m;
   w.w = 1;
-  w.elements = (culaFloat*)malloc(w.h*w.w*sizeof(culaFloat));
+  w.elements = (culaDouble*)malloc(w.h*w.w*sizeof(culaDouble));
 
-  status = culaSsyev('V', 'U', m, C.elements, m, w.elements);
-  checkStatus(status);
+  eig(C, w);
 
   printf("V = \n");
   printMatrix(C);
@@ -91,7 +137,7 @@ void culaFloatExample() {
 
 int main(int argc, char* argv[]) {
 
-  culaFloatExample();
+  eigExample();
 
   return EXIT_SUCCESS;
 }
