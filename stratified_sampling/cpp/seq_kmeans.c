ff8df3833d53e2326d127fea8eca6ae6e90f9fbc
Brian Dolhansky <bdol@hoothoot.(none)>
2014-06-20 12:29:33 -0700

    added standalone version of kmeans++

---
+++ stratified_sampling/cpp/seq_kmeans.c
@@ -0,0 +1,286 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*   File:         seq_kmeans.c  (sequential version)                        */
+/*   Description:  Implementation of simple k-means clustering algorithm     */
+/*                 This program takes an array of N data objects, each with  */
+/*                 M coordinates and performs a k-means clustering given a   */
+/*                 user-provided value of the number of clusters (K). The    */
+/*                 clustering results are saved in 2 arrays:                 */
+/*                 1. a returned array of size [K][N] indicating the center  */
+/*                    coordinates of K clusters                              */
+/*                 2. membership[N] stores the cluster center ids, each      */
+/*                    corresponding to the cluster a data object is assigned */
+/*                                                                           */
+/*   Author:  Wei-keng Liao                                                  */
+/*            ECE Department, Northwestern University                        */
+/*            email: wkliao@ece.northwestern.edu                             */
+/*   Copyright, 2005, Wei-keng Liao                                          */
+/*                                                                           */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+// Copyright (c) 2005 Wei-keng Liao
+// Copyright (c) 2011 Serban Giuroiu
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+// -----------------------------------------------------------------------------
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+
+#include "kmeans.h"
+
+
+/*----< euclid_dist_2() >----------------------------------------------------*/
+/* square of Euclid distance between two multi-dimensional points            */
+__inline static
+float euclid_dist_2(int    numdims,  /* no. dimensions */
+                    float *coord1,   /* [numdims] */
+                    float *coord2)   /* [numdims] */
+{
+    int i;
+    float ans=0.0;
+
+    for (i=0; i<numdims; i++)
+        ans += (coord1[i]-coord2[i]) * (coord1[i]-coord2[i]);
+
+    return(ans);
+}
+
+/*----< find_nearest_cluster() >---------------------------------------------*/
+__inline static
+int find_nearest_cluster(int     numClusters, /* no. clusters */
+                         int     numCoords,   /* no. coordinates */
+                         float  *object,      /* [numCoords] */
+                         float **clusters)    /* [numClusters][numCoords] */
+{
+    int   index, i;
+    float dist, min_dist;
+
+    /* find the cluster id that has min distance to object */
+    index    = 0;
+    min_dist = euclid_dist_2(numCoords, object, clusters[0]);
+
+    for (i=1; i<numClusters; i++) {
+        dist = euclid_dist_2(numCoords, object, clusters[i]);
+        /* no need square root */
+        if (dist < min_dist) { /* find the min and its array index */
+            min_dist = dist;
+            index    = i;
+        }
+    }
+    return(index);
+}
+
+double randUnif() {
+  return (double) rand() / (RAND_MAX);
+}
+
+/**
+ * Brian Dolhansky 2014.
+ * Choose an integer uniformly at random in [lower, upper]
+ */
+int randRange(int lower, int upper) {
+  double r = randUnif();
+  return (int)(r * (upper-lower) + lower);
+}
+
+
+/**
+ * Brian Dolhansky 2014.
+ * Samples a distribution specified by CDF and returns the index of that sample.
+ */
+int sampleDistribution(float* CDF, int n) {
+  double r = randUnif();
+  for (int i=0; i<n; i++) {
+    if (CDF[i] >= r) {
+      return i;
+    }
+  }
+
+  return n-1;
+}
+
+/**
+ * Brian Dolhansky 2014.
+ * kmeans++ initialization. This is O(k^2n), but greatly speeds up the actual
+ * kmeans algorithm.
+ * See http://ilpubs.stanford.edu:8090/778/1/2006-13.pdf
+ */
+void kmeanspp(float** objects,
+              int numCoords,
+              int numObjs,
+              int numClusters,
+              float** clusters)
+{
+
+  // Choose the first center uniformly at random
+  int initialIdx = randRange(0, numObjs-1);
+  for (int i=0; i<numCoords; i++) {
+    clusters[0][i] = objects[initialIdx][i];
+  }
+
+  // The density over points which we use to select the next cluster. Allocate
+  // once. Dx stores the minimum distance to an already-chosen cluster. This is
+  // updated each time we update the distance matrix.
+  float* Dx = (float *)malloc(numObjs * sizeof(float));
+  float* CDF = (float *)malloc(numObjs * sizeof(float));
+
+  // A memoization array for the distances of each point to each center
+  float** dists = (float **)malloc(numObjs * sizeof(float*));
+  dists[0] = (float *)malloc(numObjs * numClusters * sizeof(float));
+  for (int i=1; i<numObjs; i++) {
+    dists[i] = dists[i-1] + 1;
+  }
+
+  // Store the distances to the first cluster
+  for (int i=0; i<numObjs; i++) {
+    dists[i][0] = euclid_dist_2(numCoords, clusters[0], objects[i]);
+    Dx[i] = dists[i][0];
+  }
+
+  // Run kmeans++ initialization
+  for (int i=1; i<numClusters; i++) {
+
+    // We've already calculated D(x). Just form the distribution now.
+    float DxSum = 0;
+    for (int j=0; j<numObjs; j++) {
+      DxSum += Dx[j]*Dx[j];
+    }
+    for (int j=0; j<numObjs; j++) {
+      if (j==0) {
+        CDF[j] = (Dx[j]*Dx[j])/DxSum;
+      } else {
+        CDF[j] = CDF[j-1] + (Dx[j]*Dx[j])/DxSum;
+      }
+    }
+
+    // Sample the distribution for the next cluster center.
+    int objIdx = sampleDistribution(CDF, numObjs);
+    for (int c=0; c<numCoords; c++) {
+      clusters[i][c] = objects[objIdx][c];
+    }
+
+    // Update Dx
+    for (int j=0; j<numObjs; j++) {
+      float d = euclid_dist_2(numCoords, clusters[i], objects[j]);
+      if (d < Dx[j]) {
+        Dx[j] = d;
+      }
+    }
+  }
+
+  free(Dx);
+  free(dists);
+  free(CDF);
+
+}
+
+/*----< seq_kmeans() >-------------------------------------------------------*/
+/* return an array of cluster centers of size [numClusters][numCoords]       */
+float** seq_kmeans(float **objects,      /* in: [numObjs][numCoords] */
+                   int     numCoords,    /* no. features */
+                   int     numObjs,      /* no. objects */
+                   int     numClusters,  /* no. clusters */
+                   float   threshold,    /* % objects change membership */
+                   int    *membership,   /* out: [numObjs] */
+                   int    *loop_iterations)
+{
+    int      i, j, index, loop=0;
+    int     *newClusterSize; /* [numClusters]: no. objects assigned in each
+                                new cluster */
+    float    delta;          /* % of objects change their clusters */
+    float  **clusters;       /* out: [numClusters][numCoords] */
+    float  **newClusters;    /* [numClusters][numCoords] */
+
+    /* allocate a 2D space for returning variable clusters[] (coordinates
+       of cluster centers) */
+    clusters    = (float**) malloc(numClusters *             sizeof(float*));
+    assert(clusters != NULL);
+    clusters[0] = (float*)  malloc(numClusters * numCoords * sizeof(float));
+    assert(clusters[0] != NULL);
+    for (i=1; i<numClusters; i++)
+        clusters[i] = clusters[i-1] + numCoords;
+
+    /* pick first numClusters elements of objects[] as initial cluster centers*/
+    /*for (i=0; i<numClusters; i++) {*/
+        /*for (j=0; j<numCoords; j++) {*/
+            /*clusters[i][j] = objects[i][j];*/
+        /*}*/
+    /*}*/
+    kmeanspp(objects, numCoords, numObjs, numClusters, clusters);
+    std::cout << "COOL!" << std::endl;
+
+
+    /* initialize membership[] */
+    for (i=0; i<numObjs; i++) membership[i] = -1;
+
+    /* need to initialize newClusterSize and newClusters[0] to all 0 */
+    newClusterSize = (int*) calloc(numClusters, sizeof(int));
+    assert(newClusterSize != NULL);
+
+    newClusters    = (float**) malloc(numClusters *            sizeof(float*));
+    assert(newClusters != NULL);
+    newClusters[0] = (float*)  calloc(numClusters * numCoords, sizeof(float));
+    assert(newClusters[0] != NULL);
+    for (i=1; i<numClusters; i++)
+        newClusters[i] = newClusters[i-1] + numCoords;
+
+    do {
+        delta = 0.0;
+        for (i=0; i<numObjs; i++) {
+            /* find the array index of nestest cluster center */
+            index = find_nearest_cluster(numClusters, numCoords, objects[i],
+                                         clusters);
+
+            /* if membership changes, increase delta by 1 */
+            if (membership[i] != index) delta += 1.0;
+
+            /* assign the membership to object i */
+            membership[i] = index;
+
+            /* update new cluster centers : sum of objects located within */
+            newClusterSize[index]++;
+            for (j=0; j<numCoords; j++)
+                newClusters[index][j] += objects[i][j];
+        }
+
+        /* average the sum and replace old cluster centers with newClusters */
+        for (i=0; i<numClusters; i++) {
+            for (j=0; j<numCoords; j++) {
+                if (newClusterSize[i] > 0)
+                    clusters[i][j] = newClusters[i][j] / newClusterSize[i];
+                newClusters[i][j] = 0.0;   /* set back to 0 */
+            }
+            newClusterSize[i] = 0;   /* set back to 0 */
+        }
+            
+        delta /= numObjs;
+    } while (delta > threshold && loop++ < 500);
+
+    *loop_iterations = loop + 1;
+
+    free(newClusters[0]);
+    free(newClusters);
+    free(newClusterSize);
+
+    return clusters;
+}
+
